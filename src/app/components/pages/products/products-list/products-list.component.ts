5b2c094a4c662d5487cd97a6f4b54c6bcd2144c4
pavan7297 <mallapavan072@gmail.com>
2025-08-20 21:32:34 +0530

    product list up

--- src/app/components/pages/products/products-list/products-list.component.ts
+++ src/app/components/pages/products/products-list/products-list.component.ts
@@ -3,6 +3,7 @@ import { PhotoService } from '../../../../services/photo.service';
 import { GalleriaModule } from 'primeng/galleria';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ProductsService } from '../../../../services/products.service';
 
 @Component({
   selector: 'app-products-list',
@@ -14,6 +15,34 @@ import { FormsModule } from '@angular/forms';
 export class ProductsListComponent implements OnInit {
   image: any[] = [];
 
+  value: any;
+
+  images: any[] = [];
+
+  productsList: any[] = [];
+
+  currentIndex = 0;
+
+  intervalId: any;
+
+  productmenu: any[] = [
+    {
+      id: 1,
+      Title: 'veg pickels',
+      route: 'vp',
+    },
+    {
+      id: 2,
+      Title: 'Non Veg Pickle',
+      route: 'nvp',
+    },
+    {
+      id: 3,
+      Title: 'podi',
+      route: 'pd',
+    },
+  ];
+
   responsiveOptions: any[] = [
     {
       breakpoint: '1300px',
@@ -24,14 +53,78 @@ export class ProductsListComponent implements OnInit {
       numVisible: 1,
     },
   ];
-images: any[]|undefined;
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private _Serv: ProductsService
+  ) {}
 
   ngOnInit() {
+    this.getProducts();
+    this.onChangeMenu('vp');
+    this.intervalId = setInterval(() => {
+      this.nextImage();
+    }, 3000);
+  }
+
+  onChangeMenu(event: any) {
+    console.log(event);
+    switch (event) {
+      case 'vp':
+        this._Serv.getCatalog().subscribe((data: any) => {
+          this.productsList = data.veg;
+
+          console.log(this.productsList);
+        });
+        break;
+      case 'nvp':
+        this._Serv.getCatalog().subscribe((data: any) => {
+          this.productsList = data.nonVeg;
+          console.log(this.productsList);
+        });
+        break;
+      case 'pd':
+        this._Serv.getCatalog().subscribe((data: any) => {
+          this.productsList = [];
+          this.productsList = data.karapodi;
+          console.log(this.productsList);
+        });
+        break;
+      default:
+        this.getProducts();
+        break;
+    }
+  }
+
+  getProducts() {
+    this._Serv.getCatalog().subscribe((data: any) => {
+      this.productsList = data;
+      // console.log(this.productsList);
+    });
     this.photoService.getPhoneData().subscribe((data: any) => {
       console.log(data);
-      this.image = data;
+      this.images = data;
+      console.log(this.images);
     });
+  }
+
+  prevImage() {
+    this.currentIndex =
+      this.currentIndex > 0 ? this.currentIndex - 1 : this.images.length - 1;
+  }
+
+  nextImage() {
+    this.currentIndex =
+      this.currentIndex < this.images.length - 1 ? this.currentIndex + 1 : 0;
+  }
+
+  goToImage(index: number) {
+    this.currentIndex = index;
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 }
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, FormsModule,RouterLink,RouterLinkActive],
+  imports: [CommonModule, FormsModule,RouterLink,RouterLinkActive,DrawerModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   mobileMenuOpen: boolean = false;
 
+  visible2: boolean = false;
+  
   constructor() {}
 
   // Optional: You can add logic for dynamic routes or user state here
